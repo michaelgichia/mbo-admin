e6b370562921bbdc938381c0d8a0339c3b3b097e
michaelgichia <gichuru.gichia@gmail.com>
2017-12-08 15:12:24 +0300

    Create a register page.

--- app/containers/User/Register/index.js
+++ app/containers/User/Register/index.js
@@ -4,30 +4,174 @@
  *
  */
 
-import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
-// import "!!style-loader!css-loader!./Register"
-import { defaultAction } from './actions';
+import React, { PropTypes } from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { Link } from "react-router";
+import {
+  Form,
+  Input,
+  Tooltip,
+  Icon,
+  Checkbox,
+  Button,
+} from "antd";
+import "!!style-loader!css-loader!./Register.css";
 
-export class Register extends React.Component { // eslint-disable-line react/prefer-stateless-function
+const FormItem = Form.Item;
+
+export class Register extends React.Component {
+  state = {
+    confirmDirty: false,
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  handleConfirmBlur = e => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
+
+  checkPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue("password")) {
+      callback("Two passwords that you enter is inconsistent!");
+    } else {
+      callback();
+    }
+  };
+
+  checkConfirm = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(["confirm"], { force: true });
+    }
+    callback();
+  };
+
   render() {
+    const { getFieldDecorator } = this.props.form;
+    const { autoCompleteResult } = this.state;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 24 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 24 }
+      }
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0
+        },
+        sm: {
+          span: 24,
+          offset: 0
+        }
+      }
+    };
+
+
     return (
-      <div>
+      <div className="register-wrap">
+        <div className="register-header">
+          <div className="register-header-one">
+            <h1>MBO - Shop.Experience.Empower</h1>
+          </div>
+          <div className="register-header-two">
+            <h2>Register</h2>
+          </div>
+        </div>
+        <div className="register-main">
+          <Form onSubmit={this.handleSubmit}>
+            <FormItem
+              {...formItemLayout}
+              label={
+                <span>
+                  Username&nbsp;
+                  <Tooltip title="What do you want other to call you?">
+                    <Icon type="question-circle-o" />
+                  </Tooltip>
+                </span>
+              }
+            >
+              {getFieldDecorator("nickname", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please input your username!",
+                    whitespace: true
+                  }
+                ]
+              })(<Input size="large" />)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="E-mail">
+              {getFieldDecorator("email", {
+                rules: [
+                  {
+                    type: "email",
+                    message: "The input is not valid E-mail!"
+                  },
+                  {
+                    required: true,
+                    message: "Please input your E-mail!"
+                  }
+                ]
+              })(<Input size="large" />)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="Password">
+              {getFieldDecorator("password", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please input your password!"
+                  },
+                  {
+                    validator: this.checkConfirm
+                  }
+                ]
+              })(<Input size="large" type="password" />)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="Confirm Password">
+              {getFieldDecorator("confirm", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please confirm your password!"
+                  },
+                  {
+                    validator: this.checkPassword
+                  }
+                ]
+              })(<Input size="large" type="password" onBlur={this.handleConfirmBlur} />)}
+            </FormItem>
+            <FormItem {...tailFormItemLayout}>
+              <Button size="large" type="primary" htmlType="submit">
+                Register
+              </Button>
+            </FormItem>
+          </Form>
+        </div>
       </div>
     );
   }
 }
 
-Register.propTypes = {
-  // dispatch: PropTypes.func.isRequired,
-};
+Register.propTypes = {};
 
-const mapStateToProps = ({}) => ({
-
-});
-
-const mapDispatchToProps = dispatch => ({
-  dispatch
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Register);
+export default compose(
+  connect(state => ({ register: state.register })),
+  Form.create()
+)(Register);
