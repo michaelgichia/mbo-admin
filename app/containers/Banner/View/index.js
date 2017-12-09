227e6c02631c10d61e2ebf73db60b19ec11069d9
michaelgichia <gichuru.gichia@gmail.com>
2017-12-09 06:20:06 +0300

    Added banners.

--- app/containers/Banner/View/index.js
+++ app/containers/Banner/View/index.js
@@ -4,15 +4,161 @@
  *
  */
 
-import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
-// import "!!style-loader!css-loader!./View"
-import { defaultAction } from './actions';
+import React, { PropTypes } from "react";
+import {
+  Form,
+  Card,
+  Select,
+  List,
+  Icon,
+  Input,
+  Button
+} from "antd";
+import { connect } from "react-redux";
+import shortid from "shortid";
+import styles from "./styles.less";
 
-export class View extends React.Component { // eslint-disable-line react/prefer-stateless-function
+const Option = Select.Option;
+
+function handleChange(value) {
+  console.log(`selected ${value}`);
+}
+
+const { Meta } = Card;
+const Search = Input.Search;
+const pageSize = 5;
+const listData = [];
+
+for (let i = 0; i < 21; i++) {
+  listData.push({
+    id: shortid.generate(),
+    href: "/#",
+    title: `Banner ${i}`,
+    star: 1,
+    like: 200,
+    banner: "http://res.cloudinary.com/dw3arrxnf/image/upload/v1511184460/banner1_wgw1j4.png",
+    description:
+      "Here you will see very stylish banneras covers which enhance your banneras beauty more so.",
+  });
+}
+
+export class View extends React.Component {
+  fetchMore = () => {
+    this.props.dispatch({
+      type: "list/fetch",
+      payload: {
+        count: pageSize
+      }
+    });
+  };
+
   render() {
+    const loading = false;
+    const loadMore =
+      listData.length > 0 ? (
+        <div style={{ textAlign: "center", marginTop: 16 }}>
+          <Button
+            onClick={this.fetchMore}
+            style={{ paddingLeft: 48, paddingRight: 48 }}
+          >
+            {loading ? (
+              <span>
+                <Icon type="loading" /> loading...
+              </span>
+            ) : (
+              "load more"
+            )}
+          </Button>
+        </div>
+      ) : null;
+
+    const pagination = {
+      pageSize: 10,
+      current: 1,
+      total: listData.length,
+      onChange: (() => {}),
+    };
+
     return (
       <div>
+        <div style={{ marginBottom: 32 }}>
+          <Card title="Banners view">
+            <div
+              style={{
+                fontSize: 16,
+                color: "#807d7d",
+                marginBottom: 16
+              }}
+            >
+              <p>A list of banners</p>
+            </div>
+          </Card>
+        </div>
+
+        <div>
+          <Card
+            style={{ marginTop: 24, paddingTop: 16 }}
+            bordered={false}
+            bodyStyle={{ padding: "8px 32px 32px 32px" }}
+          >
+            <div className="banners-filter-div2">
+              <header>Banners Operations</header>
+            </div>
+            <div className="banners-filter-div1">
+              <label htmlFor="">Search for banner by name:</label>
+              <Search
+                placeholder="input search text"
+                onSearch={value => console.log(value)}
+                style={{ width: 200 }}
+              />
+              <label htmlFor="">Toggle banners:</label>
+              <Select defaultValue="all" style={{ width: 200 }} onChange={handleChange}>
+                <Option value="all">All Banners</Option>
+                <Option value="true">Active</Option>
+                <Option value="false">Not Active</Option>
+              </Select>
+            </div>
+          </Card>
+        </div>
+
+        <div className="ant-spin-container-wrap">
+          <Card
+            style={{ marginTop: 24, paddingTop: 50 }}
+            bordered={false}
+            bodyStyle={{ padding: "8px 32px 32px 32px" }}
+          >
+            <List
+              size="large"
+              loading={listData.length === 0 ? loading : false}
+              rowKey="id"
+              itemLayout="horizontal"
+              loadMore={loadMore}
+              dataSource={listData}
+              pagination={pagination}
+              renderItem={item => (
+                <Card
+                  style={{ width: 300 }}
+                  cover={
+                    <img
+                      alt="example"
+                      src={item.banner}
+                    />
+                  }
+                  actions={[
+                    <Icon type="setting" />,
+                    <Icon type="edit" />,
+                    <Icon type="ellipsis" />
+                  ]}
+                >
+                  <Meta
+                    title={item.title}
+                    description={item.description}
+                  />
+                </Card>
+              )}
+            />
+          </Card>
+        </div>
       </div>
     );
   }
@@ -22,12 +168,12 @@ View.propTypes = {
   // dispatch: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = ({}) => ({
+// const mapStateToProps = ({}) => ({
 
-});
+// });
 
-const mapDispatchToProps = dispatch => ({
-  dispatch
-})
+// const mapDispatchToProps = dispatch => ({
+//   dispatch
+// })
 
-export default connect(mapStateToProps, mapDispatchToProps)(View);
+export default connect(null, null)(View);
